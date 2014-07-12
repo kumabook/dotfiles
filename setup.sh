b03022b5c76965accefae7ea0ce2e86dae6ee93f
kumabook <kumabook@live.jp>
2014-07-12 09:46:03 +0900

    Fix setup.sh for emacs cask

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@
 cd -- "$(dirname -- "${0}")"
 
 ln -s `pwd`/.vimrc ~/.vimrc
-ln -s `pwd`/init.el ~/.emacs.d/init.el
 ln -s `pwd`/.zshrc  ~/.zshrc
 ln -s `pwd`/.screenrc  ~/.screenrc
 ln -s `pwd`/.pentadactylrc  ~/.pentadactylrc
 ln -s `pwd`/.pentadactyl  ~/.pentadactyl
 ln -s `pwd`/Brewfile ~/Brewfile
-ln -s `pwd`/Cask ~/Cask
 
+mkdir ~/.emacs.d/
+ln -s `pwd`/init.el ~/.emacs.d/init.el
+ln -s `pwd`/Cask ~/.emacs.d/Cask
