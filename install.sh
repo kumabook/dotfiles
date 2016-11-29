f3c7451008dca275fcf78146c5573c56f235a734
Hiroki Kumamoto <kumabook@live.jp>
2016-11-30 01:08:00 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 xcode-select --install
-curl -L git.io/nodebrew | perl - setup
-
-rbenv install 2.2.3
-gem install bundler
 
 # install ricty font
 cp -f /usr/local/Cellar/ricty/3.2.4/share/fonts/Ricty*.ttf ~/Library/Fonts/
