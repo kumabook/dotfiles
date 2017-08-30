80baf155fc7297d6f54fe0efe2b79716d45a5360
Hiroki Kumamoto <kumabook@live.jp>
2017-08-30 21:46:30 +0900

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -2,9 +2,12 @@
 cd -- "$(dirname -- "${0}")"
 
 rm ~/.vimrc
+rm ~/.tmux.conf
 rm ~/.emacs.d/init.el
+rm ~/.emacs.d/Cask
 rm ~/.zshrc
 rm ~/.screenrc
 rm ~/.pentadactylrc
 rm ~/.pentadactyl
 rm ~/Brewfile
+rm ~/.gitconfig
