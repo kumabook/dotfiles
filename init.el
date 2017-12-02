ba7c732a51257d793b084bdb68a85a4a89cf91da
Hiroki Kumamoto <kumabook@live.jp>
2017-12-02 12:40:20 +0900

    Update init.el: font

--- init.el
+++ init.el
@@ -24,17 +24,17 @@
 ;; fonts
 (when (display-graphic-p)
   (set-face-attribute 'default nil
-                      :family "Ricty Discord"
+                      :family "Ricty"
                       :height 140)
   (set-fontset-font (frame-parameter nil 'font)
                     'japanese-jisx0208
-                    (cons "Ricty Discord" "iso10646-1"))
+                    (cons "Ricty" "iso10646-1"))
   (set-fontset-font (frame-parameter nil 'font)
                     'japanese-jisx0212
-                    (cons "Ricty Discord" "iso10646-1"))
+                    (cons "Ricty" "iso10646-1"))
   (set-fontset-font (frame-parameter nil 'font)
                     'katakana-jisx0201
-                    (cons "Ricty Discord" "iso10646-1"))
+                    (cons "Ricty" "iso10646-1"))
   )
 
 
