f6c8973b76f61c8450b67dc2b56767d4322a3f1b
Hiroki Kumamoto <kumabook@live.jp>
2018-11-19 22:14:41 +0900

    Add asdf install script

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 xcode-select --install
 
+# asdf
+
+if [ -e $HOME/..asdf ]; then
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+    echo '. $HOME/.asdf/asdf.sh' >> ~/.profile
+    echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.profile
+fi
+
+asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+
+
 # install ricty font
 cp -f /usr/local/Cellar/ricty/4.0.1/share/fonts/Ricty*.ttf ~/Library/Fonts/
 fc-cache -vf
