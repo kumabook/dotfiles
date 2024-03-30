d99f2159851fdb66ca93b3ee3fb432349e0c32bf
Hiroki Kumamoto <kumabook@live.jp>
2024-03-30 21:09:11 +0900

    Use HackGen instead of Ricty

--- init.el
+++ init.el
@@ -120,17 +120,17 @@
 ;; fonts
 (when (display-graphic-p)
   (set-face-attribute 'default nil
-                      :family "Ricty"
+                      :family "HackGen Console NF"
                       :height 140)
   (set-fontset-font (frame-parameter nil 'font)
                     'japanese-jisx0208
-                    (cons "Ricty" "iso10646-1"))
+                    (cons "HackGen Console NF" "iso10646-1"))
   (set-fontset-font (frame-parameter nil 'font)
                     'japanese-jisx0212
-                    (cons "Ricty" "iso10646-1"))
+                    (cons "HackGen Console NF" "iso10646-1"))
   (set-fontset-font (frame-parameter nil 'font)
                     'katakana-jisx0201
-                    (cons "Ricty" "iso10646-1"))
+                    (cons "HackGen Console NF" "iso10646-1"))
   )
 
 ;; theme
