fe96f2247a2f9e3794d37199d126a5042d1ec0da
Hiroki Kumamoto <kumabook@live.jp>
2024-03-30 20:51:11 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ xcode-select --install
 
 # asdf
 
-if [ -e $HOME/..asdf ]; then
+if [ -e $HOME/.asdf ]; then
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf
     echo '. $HOME/.asdf/asdf.sh' >> ~/.profile
     echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.profile
@@ -10,8 +10,3 @@ fi
 
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-
-
-# install ricty font
-cp -f /opt/homebrew/Cellar/ricty/4.1.1/share/fonts/Ricty*.ttf ~/Library/Fonts/
-fc-cache -vf
